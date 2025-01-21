065027959cad141f139210e70ce8ce5d6006106c
Gabriel <gabrielzmendes1@gmail.com>
2025-01-21 17:06:09 -0300

    Use Case and Design Patterns - Repository Interface

---
+++ src/repositories /inMemory/in-memory-users-repository.ts
@@ -0,0 +1,25 @@
+import { Prisma, User } from '@prisma/client';
+import { usersRepository } from '../users-repository-interface';
+
+export class InMemoryUsersRepository implements usersRepository {
+  public users: User[] = [];
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    const user: User = {
+      id: (this.users.length + 1).toString(),
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    };
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = this.users.find((user) => user.email === email);
+    return user || null;
+  }
+}
--- src/useCases/register.ts
+++ src/useCases/register.ts
@@ -1,4 +1,4 @@
-import { prisma } from '@/lib/prisma';
+import { usersRepository } from '@/repositories /users-repository-interface';
 import { hash } from 'bcryptjs';
 
 interface RegisterUseCaseRequest {
@@ -8,13 +8,13 @@ interface RegisterUseCaseRequest {
 }
 
 export class RegisterUseCase {
-  constructor(private usersRepository: any) {}
+  constructor(private usersRepository: usersRepository) {}
 
   async execute({ name, email, password }: RegisterUseCaseRequest) {
     const password_hash = await hash(password, 6);
 
-    if (await prisma.user.findUnique({ where: { email } })) {
-      throw new Error('E-mail already exists.');
+    if (await this.usersRepository.findByEmail(email)) {
+      throw new Error(`E-mail "${email}" already exists`);
     }
 
     await this.usersRepository.create({
