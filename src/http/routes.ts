31cf526ebd49ce2a5103c6ba5cabeb4aa66b1636
Gabriel <gabrielzmendes1@gmail.com>
2025-02-25 16:31:53 -0300

    Implementing Use Cases - Tests and Authentication Controller

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -3,4 +3,6 @@ import { register } from './controllers/register.controller';
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register);
+
+  app.post('/sessions', authenticate);
 }
