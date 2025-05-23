0a45ef9de0bb8c2b3adff3ca65e6429c4cb019d9
Gabriel Mendes <gabrielzmendes1@gmail.com>
2025-05-23 15:24:12 -0300

    Implementing use cases - Check-in validate use case

--- src/repositories/inMemory/in-memory-check-ins-repository.ts
+++ src/repositories/inMemory/in-memory-check-ins-repository.ts
@@ -4,35 +4,44 @@ import { CheckInsRepository } from '../check-ins-repository';
 import dayjs from 'dayjs';
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
+  async findById(id: string) {
+    const checkIn = this.items.find((item) => item.id === id);
+
+    if (!checkIn) {
+      return null;
+    }
+
+    return checkIn;
+  }
   public items: CheckIn[] = [];
 
   async findByUserIdOnDate(userId: string, date: Date) {
-    const startOfTheDay = dayjs(date).startOf('date')
-    const endOfTheDay = dayjs(date).endOf('date')
+    const startOfTheDay = dayjs(date).startOf('date');
+    const endOfTheDay = dayjs(date).endOf('date');
 
     const checkInOnSameDate = this.items.find((checkIn) => {
-      const checkInDate = dayjs(checkIn.created_at)
+      const checkInDate = dayjs(checkIn.created_at);
       const isOnSameDate =
-        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay);
 
-      return checkIn.userId === userId && isOnSameDate
-    })
+      return checkIn.userId === userId && isOnSameDate;
+    });
 
     if (!checkInOnSameDate) {
-      return null
+      return null;
     }
 
-    return checkInOnSameDate
+    return checkInOnSameDate;
   }
 
-    async findManyByUserId(userId: string, page: number): Promise<CheckIn[]> {
-      return this.items
-        .filter(item => item.userId === userId)
-        .slice((page - 1) * 20, page * 20)
-    }
+  async findManyByUserId(userId: string, page: number) {
+    return this.items
+      .filter((item) => item.userId === userId)
+      .slice((page - 1) * 20, page * 20);
+  }
 
-    async countByUserId(userId: string): Promise<number> {
-      return this.items.filter(item => item.userId === userId).length
+  async countByUserId(userId: string) {
+    return this.items.filter((item) => item.userId === userId).length;
   }
 
   async create(data: Prisma.CheckInUncheckedCreateInput) {
@@ -46,6 +55,17 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
     this.items.push(checkIn);
 
+    return checkIn;
+  }
+
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.items.findIndex((item) => item.id === checkIn.id);  
+
+    if (checkInIndex >= 0) {
+      this.items[checkInIndex] = checkIn;
+    } else {
+      this.items.push(checkIn);
+    }
     return checkIn;
   }
 }
